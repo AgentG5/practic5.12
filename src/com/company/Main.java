16937e4667bd89b53cd9f27db5ec1909ddf749a1
AgentG5 <maksum3000@gmail.com>
2022-05-10 22:29:57 +0300

    v1

---
+++ src/com/company/Main.java
@@ -0,0 +1,146 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+
+        //zavdanja 1
+//                int array[] = new int[50];
+//                for (int i = 1, j = 0; i <= 99; i = i + 2, j++) {
+//                    System.out.println(array[j] = i);
+//                }
+
+
+
+        //zavdanja 2
+
+
+//        int array[] = new int[20];
+//        for (int i = 0; i < 20; i++) {
+//            array[i] = (int) Math.round(Math.random() * 9);
+//            System.out.print(array[i]);
+//        }
+//        System.out.print(" ");
+//
+//        for (int j = 0; j < array.length; j++) {
+//            if (j % 2 != 0) {
+//                array[j] = 0;
+//            }
+//            System.out.print(array[j]);
+//
+//
+//        }
+        //zavdanja 3
+
+
+
+
+//                double sered_1 = 0, sered_2 = 0, sered_3 = 0;
+//                int array[] = new int[5];
+//                for (int a = 0; a < 5; a++) {
+//                    array[a] = (int) Math.round(Math.random() * 5);
+//                    System.out.print(array[a] + " ");
+//                    sered_1 = sered_1 + array[a];
+//
+//                }
+//                System.out.println("  " + sered_1 / 5);
+//                int array_2[] = new int[5];
+//                for (int b = 0; b < 5; b++) {
+//                    array_2[b] = (int) Math.round(Math.random() * 5);
+//                    System.out.print(array_2[b] + " ");
+//                    sered_2 = sered_2 + array_2[b];
+//                }
+//                System.out.println("  " + sered_2 / 5);
+//                int array_3[] = new int[5];
+//                for (int c = 0; c < 5; c++) {
+//                    array_3[c] = (int) Math.round(Math.random() * 5);
+//                    System.out.print(array[c] + " ");
+//                    sered_3 = sered_3 + array_3[c];
+//                }
+//                System.out.println("  " + sered_3 / 5);
+//
+//                System.out.println(Math.max(sered_1 / 5 , Math.max(sered_2 / 5, sered_3 / 5)));
+//
+//                if(sered_1 == sered_2 && sered_1 == 3 && sered_2 == sered_3){
+//                    System.out.println("Усі значення середніх арифметичних є рівними");
+//                }
+
+
+        //zavdanja 4
+
+
+
+
+
+//                int array_1[] = new int[10];
+//                for(int a = 0; a < 10; a++){
+//                    array_1[a] = (int) Math.round(Math.random()* 10);
+//                    System.out.print(array_1[a]);
+//                }
+//                System.out.print(" ");
+//
+//                int array_2[] = new int[10];
+//                for(int a = 0; a < 10; a++){
+//                    array_2[a] = (int) Math.round(Math.random()* 10);
+//                    System.out.print(array_2[a]);
+//                }
+//                System.out.println();
+//                int array_3[] = new int[10];
+//                for(int a = 0; a < 10; a++){
+//                    array_3[a] =  array_1[a] + array_2[a];
+//                    System.out.print(array_3[a] + " ");
+//                }
+
+
+        //zavdanja 5
+
+
+//        int parni = 0;
+//        int array[] = new int[15];
+//        for(int i = 0; i < 15; i++){
+//            array[i] = (int) Math.round(Math.random()*10);
+//            System.out.println(array[i]);
+//
+//            if(array[i] % 2 == 0){
+//                parni++;
+//
+//            }
+//
+//        }
+//        System.out.println();
+//        System.out.println("Парні : " + parni);
+
+
+        //zavdanja 6
+
+
+//        int[][]t_array = new int[15][];
+//        t_array[0] = new int [5];
+//        t_array[1] = new int [5];
+//        t_array[2] = new int [5];
+//        t_array[3] = new int [5];
+//
+//        t_array[4] = new int [8];
+//        t_array[5] = new int [8];
+//        t_array[6] = new int [8];
+//        t_array[7] = new int [8];
+//
+//        t_array[8] = new int [3];
+//        t_array[9] = new int [3];
+//        t_array[10] = new int [3];
+//        t_array[11] = new int [3];
+//
+//        t_array[12] = new int [9];
+//        t_array[13] = new int [9];
+//        t_array[14] = new int [9];
+//
+//        for(int i = 0;i < t_array.length; i++){
+//            for(int j = 0; j< t_array[i].length; j++){
+//                t_array[i][j] = (int) Math.round(Math.random()*15);
+//                System.out.println(t_array[i][j]+ " ");
+//            }
+//            System.out.println(" ");
+//        }
+    }
+}
